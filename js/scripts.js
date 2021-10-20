f759f3b8bb81d5ca3f80a54c3ecfb0a3edfdd5a6
Matt Luker <jamesmattluker@gmail.com>
2021-10-20 13:08:11 -0600

    add .ready(function) and submit function in js
    Co-authored-by: Colton Thatcher <colt.thatcher303@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-$(document)
+$(document).ready(function() {
+  $("#formOne").sumbit(function() {
+    event.preventDefault();
+    const yourInput = $("input#your-text").val();
+
+    $(".your-text").text(yourInput);
+
+
+
+  });
+});
